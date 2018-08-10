fd2e0babea7f5ded9a329f7df00b178cb92199c4
MPewny <41195328+MPewny@users.noreply.github.com>
2018-08-10 15:19:53 +0200

    Create IPBlocker.php

---
+++ IPBlocker.php
@@ -0,0 +1,91 @@
+<?php
+/***************************************************
+*                                                  *
+*                IPBlocker v1.0                    *
+*                                                  *
+*        GitHub: https://github.com/MPewny         *
+*                                                  *
+*  Use:                                            *
+*    <?php require('[file path]/IPBlocker.php');   *
+*      [functions You want to use]; ?>             *
+*                                                  *
+*  IPBlocker is an Open Source software            *
+*    don't use it for commercial reasons           *
+*      that mean:                                  *
+*       -don't sell it                             *
+*     -don't tell it's Your                        *
+*    -if You use it on commercial website          *
+*              publish My github link              *
+***************************************************/
+
+require("IPBsettings.php");
+function ipb_ip{
+  if(isset($HTTP_X_FORWARDED_FOR)){
+	 $ip = $HTTP_X_FORWARDED_FOR;
+	 }elseif(isset($HTTP_X_FORWARDED)){
+	 $ip = $HTTP_X_FORWARDED;
+	 }elseif(isset($HTTP_VIA)){
+	 $ip = $HTTP_VIA;
+	 }elseif(isset($HTTP_CLIENT_IP)){
+	 $ip = $HTTP_CLIENT_IP;
+	 }elseif(isset($FORWARDED_FOR_IP)){
+	 $ip = $FORWARDED_FOR_IP;
+	 }else{
+   $ip = $_SERVER['REMOTE_ADDR'];
+	 }
+$checked=0;
+ while($checked == count($iplist)){
+   if($ip == $banip[$checked]){
+	 header ('Location:'.$ipbanpage);
+	 die;
+	 }else{
+	 $checked = $chenced + 1;
+	 die;
+	 }
+ }
+}
+function ipb_country(){
+$ip = $_SERVER['REMOTE_ADDR'];
+$country = 'http://api.hostip.info/country.php?ip='.$ip;
+  if(isset($CountryName[$country])){
+	$_SESSION['banned'] = true;
+	header("Location: ".$ccbanpage);
+	}
+}
+
+function ipb_proxy(){
+    $proxyheaders = array(
+        'HTTP_VIA',
+        'HTTP_X_FORWARDED_FOR',
+        'HTTP_FORWARDED_FOR',
+        'HTTP_X_FORWARDED',
+        'HTTP_FORWARDED',
+        'HTTP_CLIENT_IP',
+        'HTTP_FORWARDED_FOR_IP',
+        'VIA',
+        'X_FORWARDED_FOR',
+        'FORWARDED_FOR',
+        'X_FORWARDED',
+        'FORWARDED',
+        'CLIENT_IP',
+        'FORWARDED_FOR_IP',
+        'HTTP_PROXY_CONNECTION'
+    );
+    foreach($proxyheaders as $x){
+        if (isset($_SERVER[$x])){
+				 header("Location:".$proxybanpage);
+				}
+				
+    }
+}
+function ipb_trustedip(){
+$ip = $_SERVER['REMOTE_ADDR'];
+ foreach($trustip as $x){
+  if($ip == $trustip[$x]){
+    $access = true;
+  }else{
+    header("Location:".$UntrustedIPpage);
+	}
+ }
+}
+?>
